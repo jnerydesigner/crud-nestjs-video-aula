fcf16178ecc2402b8a69f1c4f8e79cc0d9970efd
jnerydesigner <jander.webmaster@gmail.com>
2021-07-15 10:43:44 -0400

    add config dotenv for nestjs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,21 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     MongooseModule.forRoot(
-      'mongodb+srv://product-api:product1234@cluster0.fgvik.mongodb.net/products',
+      `mongodb+srv://${process.env.USER_MONGO_DB}:${process.env.PASSWORD_MONGO_DB}@cluster0.fgvik.mongodb.net/products`,
       {
         useFindAndModify: false,
       },
     ),
     MongooseModule.forRoot(
-      'mongodb+srv://product-api:product1234@cluster0.fgvik.mongodb.net/type',
+      `mongodb+srv://${process.env.USER_MONGO_DB}:${process.env.PASSWORD_MONGO_DB}@cluster0.fgvik.mongodb.net/type`,
       {
         useFindAndModify: false,
       },
